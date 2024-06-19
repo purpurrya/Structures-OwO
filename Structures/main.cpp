b69c46a0f23a0351efb85e84dbb65b4381da4ba1
purpurrya <anastasia.tikhonenko.meow@gmail.com>
2024-06-20 01:52:59 +1000

    delete comments in main file

--- Structures/main.cpp
+++ Structures/main.cpp
@@ -3,18 +3,15 @@
 #include "tests.h"
 
 int main() {
-    std::vector<int> v = {10, 2, 3, 4, 6};
-    Heap h(v);
+    zero_elements();
 
-    //zero_elements();
+    popping_zero_elements();
 
-    //popping_zero_elements();
+    random_elements();
 
-    //random_elements();
+    first_case_popping_random_elements();
 
-    //first_case_popping_random_elements();
-
-    //second_case_popping_random_elements();
+    second_case_popping_random_elements();
 
     third_case_popping_random_elements();
 }
